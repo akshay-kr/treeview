4a26623353c72064ab2560b52341f8a767309b30
akshay1706 <akshaykumar.cse@gmail.com>
2016-06-19 02:51:55 +0530

    Created factory form UI with validation and error messaging

--- client/controller.js
+++ client/controller.js
@@ -1,6 +1,41 @@
 'use strict';
 /*jshint node:true, quotmark:false*/
-/*global angular, console, chrome, baseUrl, _, $, classifications, window*/
+/*global angular, console, baseUrl*/
 var app = angular.module("treeViewApp", ['ngToast']);
-app.controller('mainController', function ($scope, $http, ngToast) {
-	});
+app.controller('mainController', function($scope, $http, ngToast) {
+	$scope.showFactory = false;
+
+});
+app.directive('factoryForm', ['$compile', function($compile) {
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs) {
+			var rootButton = $("#root-button");
+			var offset = element.offset();
+			rootButton.on('mouseover', function() {
+				scope.showFactory = true;
+				scope.$apply();
+			});
+			rootButton.on('mouseleave', function() {
+				scope.showFactory = false;
+				scope.$apply();
+			});
+			element.on('mouseover',function(){
+				scope.showFactory = true;
+				scope.$apply();
+			});
+			element.on('mouseleave',function(){
+				scope.showFactory = false;
+				scope.$apply();
+				clearForm();
+			});
+			function clearForm() {
+				scope.factoryName="";
+				scope.min = "";
+				scope.max="";
+				scope.factoryForm.$setPristine(true);
+				scope.$apply();
+			}
+		}
+	};
+}]);
