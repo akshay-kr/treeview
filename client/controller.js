28eec8eea0592f36041cc66d6418b7f28962a13b
akshay1706 <akshaykumar.cse@gmail.com>
2016-06-18 17:08:52 +0530

    Integrated socket.io and other UI libs for client

---
+++ client/controller.js
@@ -0,0 +1,6 @@
+'use strict';
+/*jshint node:true, quotmark:false*/
+/*global angular, console, chrome, baseUrl, _, $, classifications, window*/
+var app = angular.module("treeViewApp", ['ngToast']);
+app.controller('mainController', function ($scope, $http, ngToast) {
+	});
